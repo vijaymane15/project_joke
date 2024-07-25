6592bd3cde763936a82b258c7850d96f69c840ba
Vijay Mane <141431816+vijaymane15@users.noreply.github.com>
2024-07-25 19:16:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
